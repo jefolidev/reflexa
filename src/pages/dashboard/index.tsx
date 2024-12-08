abb5f2abea1f0c11fc4446db44caf4ef5b7cce85
jefolidev <jefoliveira279@gmail.com>
2024-12-08 11:17:35 -0300

    feat/criado sessão de objetivos na dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,8 +2,10 @@ import turnArrowIcon from '../../assets/arrow-cycle.svg'
 import arrowLeft from '../../assets/arrow-left.svg'
 import arrowRight from '../../assets/arrow-right.svg'
 import check from '../../assets/check.svg'
+import checkDefaultEmoji from '../../assets/checklist/check-default.svg'
 import happy from '../../assets/emotions/happy-emotion.svg'
 import pencilEmoji from '../../assets/pencil-emoji.png'
+import plus from '../../assets/plus.svg'
 
 export function DashboardPage() {
   return (
@@ -28,7 +30,7 @@ export function DashboardPage() {
             </button>
           </div>
         </header>
-        <main className="flex flex-col">
+        <main className="flex flex-col gap-8">
           <section className="flex justify-around gap-10 h-64 ">
             <div className=" h-full flex-1 bg-yellow-300 flex flex-col items-center justify-evenly rounded-2xl p-2  ">
               <header>
@@ -99,6 +101,54 @@ export function DashboardPage() {
                 </button>
               </header>
             </div>
+          </section>
+          <section className="flex flex-col gap-4">
+            <header>
+              <h2 className="font-monts">Objetivos que impactam</h2>
+            </header>
+            <main className="flex h-72 gap-6">
+              <div className="flex flex-col w-72">
+                <div className="flex justify-between items-center bg-stone-900 p-3.5 rounded-lg hover:bg-stone-800 hover:cursor-pointer">
+                  <img src={checkDefaultEmoji} alt="" />
+                  <span className="font-poppins text-sm text-white ">
+                    Arrumar a cama
+                  </span>
+                  <div className="bg-green-700/70 rounded-full w-12 flex justify-center ">
+                    <span className="font-monts  text-green-300 font-bold text-sm p-1 ">
+                      5
+                    </span>
+                  </div>
+                </div>s
+              </div>
+              <div className=" h-full  bg-stone-900 flex flex-col justify-between items-enter rounded-2xl px-6 py-8">
+                <header>
+                  <h2>Adicionar task rápida</h2>
+                </header>
+                <footer className="flex gap-2 self-end">
+                  <button
+                    type="button"
+                    className="hover:cursor-pointer hover:bg-gray-200"
+                  >
+                    <img src={plus} alt="" className="p-1" />
+                  </button>
+                </footer>
+              </div>
+              <div className="h-full flex-2 bg-stone-900 flex rounded-2xl px-6 py-8">
+                <header className="flex h-14 w-full justify-between items-center">
+                  <div>
+                    <h3 className="text-stone-400">
+                      Objetivos concluídos nessa semana
+                    </h3>
+                    <span className="flex text-white text-xl font-monts font-bold items-center gap-2 ">
+                      5
+                      <p className="w-12 h-5 text-red-300 bg-red-600/70 text-sm text-center rounded-full">
+                        +25%
+                      </p>
+                    </span>
+                  </div>
+                </header>
+              </div>
+            </main>
           </section>
         </main>
       </div>
