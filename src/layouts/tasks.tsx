85ee442f59742c5f8fcfa877af8b72931b4d4841
jefolidev <jefoliveira279@gmail.com>
2024-12-18 17:37:06 -0300

    refactor/criado componentização para as abas de navegação de tasks

--- src/layouts/tasks.tsx
+++ src/layouts/tasks.tsx
@@ -1,21 +1,22 @@
-import { NavLink, Outlet } from 'react-router'
+import { Outlet } from 'react-router'
+import { TaskTab } from '../components/tab'
+import { useModal } from '../hooks/useModal'
+import { NewGoalModal } from '../pages/goals/components/modals/new-task-modal'
+
 import plus from '../assets/common-assets/plus.svg'
 import completedIcon from '../assets/navbar-icons/completed.svg'
 import otherWeeksIcon from '../assets/navbar-icons/other-weeks.svg'
 import pendingIcon from '../assets/navbar-icons/sandtime.svg'
 import uncompletedIcon from '../assets/navbar-icons/uncompleted.svg'
-import { useModal } from '../hooks/useModal'
-import { NewGoalModal } from '../pages/goals/components/modals/new-task-modal'
 
 /* 
     TODO - 
         1. Adicionar o nome da rota atual no header 'Dashboard / Objetivos / 'nome_da_rota'
 */
 
-const activeTabStyle = 'border-b-2 border-white '
-
 export function TasksLayout() {
-  const { switchTheCreateGoalModalState } = useModal()
+  const { isCreateGoalModalVisibile, switchTheCreateGoalModalState } =
+    useModal()
 
   return (
     <div className=" bg-zinc-800 w-screen h-screen flex-col p-12 relative">
@@ -27,7 +28,7 @@ export function TasksLayout() {
         <img src={plus} alt="" />
       </button>
 
-      <NewGoalModal />
+      {isCreateGoalModalVisibile && <NewGoalModal />}
 
       <header className="mb-4">
         <span className="text-stone-500 text-sm font-poppins inline-flex gap-1">
@@ -41,58 +42,22 @@ export function TasksLayout() {
         </header>
         <div className="w-full bg-stone-700 h-px mt-4 mb-2 " />
         <nav className="flex gap-2">
-          <NavLink
-            to={'/tasks/pending'}
-            className={({ isActive }) =>
-              isActive ? activeTabStyle : 'border-b-0'
-            }
-          >
-            <div className="flex gap-3 items-center p-2">
-              <img src={pendingIcon} alt="" />
-              <span className="font-monts text-white text-sm font-semibold">
-                Pendentes
-              </span>
-            </div>
-          </NavLink>
-          <NavLink
-            to={'/tasks/uncompleted'}
-            className={({ isActive }) =>
-              isActive ? activeTabStyle : 'border-b-0'
-            }
-          >
-            <div className="flex gap-3 items-center p-2">
-              <img src={uncompletedIcon} alt="" className="w-6" />
-              <span className="font-monts text-white text-sm font-semibold">
-                Não Concluídos
-              </span>
-            </div>
-          </NavLink>
-          <NavLink
-            to={'/tasks/completed'}
-            className={({ isActive }) =>
-              isActive ? activeTabStyle : 'border-b-0'
-            }
-          >
-            <div className="flex gap-3 items-center p-2">
-              <img src={completedIcon} alt="" className="w-6" />
-              <span className="font-monts text-white text-sm font-semibold">
-                Concluídos
-              </span>
-            </div>
-          </NavLink>
-          <NavLink
-            to={'/tasks/other-weeks'}
-            className={({ isActive }) =>
-              isActive ? activeTabStyle : 'border-b-0'
-            }
-          >
-            <div className="flex gap-3 items-center p-2">
-              <img src={otherWeeksIcon} alt="" className="w-6" />
-              <span className="font-monts text-white text-sm font-semibold">
-                Outras semanas
-              </span>
-            </div>
-          </NavLink>
+          <TaskTab to="/tasks/pending" icon={pendingIcon} label="Pendentes" />
+          <TaskTab
+            to="/tasks/uncompleted"
+            icon={uncompletedIcon}
+            label="Não Concluídos"
+          />
+          <TaskTab
+            to="/tasks/completed"
+            icon={completedIcon}
+            label="Concluídos"
+          />
+          <TaskTab
+            to="/tasks/other-weeks"
+            icon={otherWeeksIcon}
+            label="Outras semanas"
+          />
         </nav>
         <div>
           <Outlet />
